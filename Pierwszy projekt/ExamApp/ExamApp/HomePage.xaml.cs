d112ffe77ad5744d8e6f60c27582c40d7ba9e209
kacper <k4cper.petelicki@gmail.com>
2024-03-12 13:31:45 +0100

    new

--- Pierwszy projekt/ExamApp/ExamApp/HomePage.xaml.cs
+++ Pierwszy projekt/ExamApp/ExamApp/HomePage.xaml.cs
@@ -9,13 +9,25 @@ using Xamarin.Forms.Xaml;
 
 namespace ExamApp
 {
+
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class HomePage : ContentPage
     {
+        private string _login;
         public HomePage( string login)
         {
-            
+            _login = login;
             InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ListPage());
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new InfoPage(_login));
         }
     }
 }
