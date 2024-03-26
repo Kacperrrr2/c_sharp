3810d7c488af247d6b81a7da6b4d4e8c697420ad
Test <test@test.pl>
2024-03-26 11:42:43 +0100

    add

---
+++ Pierwszy projekt/TappedApp/TappedApp/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TappedApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            TabbedPage tabbedPage= new TabbedPage();
+            tabbedPage.Children.Add(new FirstPage() { Title = "Pierwsza" });
+            tabbedPage.Children.Add(new SecondPage() { Title = "Druga" });
+            tabbedPage.Children.Add(new ThirdPage() { Title = "Trzecia" });
+            tabbedPage.Children.Add(new ThirdPage() { Title = "4" });
+            tabbedPage.Children.Add(new ThirdPage() { Title = "555" });
+
+            MainPage = tabbedPage;
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
